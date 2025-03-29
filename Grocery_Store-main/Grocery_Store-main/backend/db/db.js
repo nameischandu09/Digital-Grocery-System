191ca5fd0fc34ecd3652240368f3beb0321d6e6a
nameischandu09 <140332143+nameischandu09@users.noreply.github.com>
2025-03-30 00:03:54 +0530

    Delete Grocery_Store-main directory

--- Grocery_Store-main/Grocery_Store-main/backend/db/db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose")
-let dbConfig = async () => {
-    const instance = await mongoose.connect(
-        `${process.env.MONGODB_URL}${process.env.DB_NAME}?retryWrites=true&w=majority`
-    );
-    return instance;
-}
-module.exports = {dbConfig};
